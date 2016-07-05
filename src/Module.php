9c8554e9ab58f1292a76c9f7fe9cae0cf5d14551
popovsergiy <popow.sergiy@gmail.com>
2016-07-05 15:36:17 +0300

    Block has been rebuild

--- src/Module.php
+++ src/Module.php
@@ -13,7 +13,7 @@ use Zend\Mvc\ModuleRouteListener;
 use Zend\ModuleManager\ModuleManager;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 
-class Module implements AutoloaderProviderInterface
+class Module
 {
     public function init(ModuleManager $moduleManager)
     {
@@ -38,20 +38,6 @@ class Module implements AutoloaderProviderInterface
 
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return [
-            /*'Zend\Loader\ClassMapAutoloader' => [
-                __DIR__ . '/autoload_classmap.php',
-            ],*/
-            'Zend\Loader\StandardAutoloader' => [
-                'namespaces' => [
-                    __NAMESPACE__ => str_replace('\\', '/', __DIR__ . '/src/' . __NAMESPACE__),
-                ],
-            ],
-        ];
+        return include __DIR__ . '/../config/module.config.php';
     }
 }
